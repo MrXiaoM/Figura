938ed867f2b8effe256668c5c82436f3320b5e7f
人間工作 <coolxiaom95@gmail.com>
2024-12-12 03:13:50 +0800

    supports load `.moon` directly by command

--- common/src/main/java/org/figuramc/figura/commands/LoadCommand.java
+++ common/src/main/java/org/figuramc/figura/commands/LoadCommand.java
@@ -29,8 +29,12 @@ class LoadCommand {
             Path p = LocalAvatarFetcher.getLocalAvatarDirectory().resolve(Path.of(str));
 
             // try to load avatar
-            AvatarManager.loadLocalAvatar(p);
-            context.getSource().figura$sendFeedback(new FiguraText("command.load.loading"));
+            if (p.toFile().isFile()) {
+                AvatarManager.loadMoonAvatar(p);
+            } else {
+                AvatarManager.loadLocalAvatar(p);
+            }
+            context.getSource().figura$sendFeedback(FiguraText.of("command.load.loading"));
             return 1;
         } catch (Exception e) {
             context.getSource().figura$sendError(new FiguraText("command.load.invalid", str));
