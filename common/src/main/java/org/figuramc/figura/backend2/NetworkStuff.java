a1de148028ed4e80143f0924629d8930c8791b48
人間工作 <coolxiaom95@gmail.com>
2024-12-12 05:14:30 +0800

    add `canUpload` to limits

--- common/src/main/java/org/figuramc/figura/backend2/NetworkStuff.java
+++ common/src/main/java/org/figuramc/figura/backend2/NetworkStuff.java
@@ -77,6 +77,8 @@ public class NetworkStuff {
 
     public static Version latestVersion;
 
+    private static boolean uploadPermission = false;
+
     //limits
     private static final RefilledNumber
             uploadRate = new RefilledNumber(),
@@ -295,6 +297,8 @@ public class NetworkStuff {
 
             JsonObject limits = json.getAsJsonObject("limits");
             maxAvatarSize = limits.get("maxAvatarSize").getAsInt();
+            JsonElement canUpload = limits.get("canUpload"); // default false
+            uploadPermission = canUpload != null && canUpload.getAsBoolean();
         });
     }
 
@@ -563,7 +567,7 @@ public class NetworkStuff {
     }
 
     public static boolean canUpload() {
-        return isConnected() && uploadRate.check();
+        return isConnected() && uploadPermission && uploadRate.check();
     }
 
     public static int getSizeLimit() {
