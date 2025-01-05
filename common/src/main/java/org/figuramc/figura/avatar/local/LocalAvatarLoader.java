a37e4b36fad881e77d55296fd2e0e0a3cd35517b
人間工作 <coolxiaom95@gmail.com>
2025-01-05 14:18:17 +0800

    fix load both from server and local

--- common/src/main/java/org/figuramc/figura/avatar/local/LocalAvatarLoader.java
+++ common/src/main/java/org/figuramc/figura/avatar/local/LocalAvatarLoader.java
@@ -95,6 +95,14 @@ public class LocalAvatarLoader {
      * @param path - the file/folder for loading the avatar
      */
     public static void loadAvatar(Path path, UserData target) {
+        loadAvatar(path, target, null);
+    }
+    /**
+     * Loads an NbtCompound from the specified path
+     *
+     * @param path - the file/folder for loading the avatar
+     */
+    public static void loadAvatar(Path path, UserData target, Runnable done) {
         loadError = null;
         loadState = LoadState.UNKNOWN;
         resetWatchKeys();
@@ -104,8 +112,12 @@ public class LocalAvatarLoader {
         }
         lastLoadedPath = path;
 
-        if (path == null || target == null)
+        if (path == null || target == null) {
+            if (done != null) {
+                done.run();
+            }
             return;
+        }
 
         addWatchKey(path, KEYS::put);
 
@@ -164,6 +176,7 @@ public class LocalAvatarLoader {
                 FiguraMod.LOGGER.error("Failed to load avatar from " + finalPath, e);
                 FiguraToast.sendToast(new FiguraText("toast.load_error"), new FiguraText("gui.load_error." + LocalAvatarLoader.getLoadState()), FiguraToast.ToastType.ERROR);
             }
+            if (done != null) done.run();
         });
     }
     private static void loadResources(CompoundTag nbt, ListTag pathsTag, Path parentPath) {
