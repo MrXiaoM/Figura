05565c2cbe38c89940553aaf10776ae69f9368d3
人間工作 <coolxiaom95@gmail.com>
2025-01-24 03:16:44 +0800

    refresh limits when reconnect

--- common/src/main/java/org/figuramc/figura/backend2/AuthHandler.java
+++ common/src/main/java/org/figuramc/figura/backend2/AuthHandler.java
@@ -19,6 +19,7 @@ public class AuthHandler {
     public static void auth(boolean reAuth) {
         NetworkStuff.async(() -> {
             if (!reAuth && NetworkStuff.isConnected()) {
+                NetworkStuff.setLimits();
                 AvatarManager.fetchAvatarForLocal();
                 return;
             }
